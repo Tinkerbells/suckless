6d9b2d3bdb7dec2f6eb6344270fa4d0a90dc1fc7
untermensch <daniel_unter_work@pm.me>
2022-06-19 12:48:42 +0300

    add dwmblocks

---
+++ dwmblocks/config.h
@@ -0,0 +1,19 @@
+#define CMDLENGTH 45
+#define DELIMITER " | "
+#define CLICKABLE_BLOCKS
+
+const Block blocks[] = {
+	/* Block script name         time       signal */
+	BLOCK("sb-music",            0,         12),
+	BLOCK("sb-dollar",           1800,      11),
+	BLOCK("sb-bitcoin",          1800,      10),
+	BLOCK("sb-network",    	     1,         9),
+	BLOCK("sb-memory",    	     1,         8),
+	BLOCK("sb-cpu-temp",         1,         7),
+	BLOCK("sb-bluetooth",        600,       6),
+	BLOCK("sb-volume",    	     0, 	5),
+	BLOCK("sb-keyboard-layout",  0,      	4),
+	BLOCK("sb-wifi",    	     10,        3),
+	BLOCK("sb-battery",          30, 	2),
+	BLOCK("sb-time",    	     1, 	1),
+};
