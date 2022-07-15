259152e3f4e0eedfa292daa4334c4678ee136e6d
untermensch <daniel_unter_work@pm.me>
2022-07-15 17:21:41 +0300

    update config, change delimiter to " "

--- dwmblocks/config.h
+++ dwmblocks/config.h
@@ -1,5 +1,5 @@
-#define CMDLENGTH 45
-#define DELIMITER " | "
+#define CMDLENGTH 60 
+#define DELIMITER " "
 #define CLICKABLE_BLOCKS
 
 const Block blocks[] = {
@@ -8,12 +8,13 @@ const Block blocks[] = {
 	BLOCK("sb-dollar",           3600,      11),
 	BLOCK("sb-bitcoin",          3600,      10),
 	BLOCK("sb-network",    	     1,         9),
-//      BLOCK("sb-memory",    	     1,         8),
-//	BLOCK("sb-cpu-temp",         1,         7),
+  BLOCK("sb-memory",    	     1,         8),
+	BLOCK("sb-cpu-temp",         1,         7),
+	BLOCK("sb-cpu-freq",         1,         7),
 	BLOCK("sb-bluetooth",        1800,      6),
-	BLOCK("sb-volume",    	     0, 	5),
-	BLOCK("sb-keyboard-layout",  1, 	4),
-	BLOCK("sb-wifi",    	     10,        3),
-	BLOCK("sb-battery",          30, 	2),
-	BLOCK("sb-time",    	     1, 	1),
+	BLOCK("sb-volume",    	     0, 	      5),
+	BLOCK("sb-keyboard-layout",  1, 	      4),
+	BLOCK("sb-wifi",    	       10,        3),
+	BLOCK("sb-time",    	       1,	        1),
+	BLOCK("sb-battery",          30, 	      2),
 };
